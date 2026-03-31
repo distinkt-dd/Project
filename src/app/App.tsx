f9ee71e1857f01db300c272a71c9d84155b1e468
--Dmitriy <120382345+distinkt-dd@users.noreply.github.com>
2026-03-31 12:33:21 +0500

    feat(app): подключение заготовок под стили

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { SecondPage } from '@pages/second';
 import './styles/index.css';
 import { MainPage } from '@pages/main';
+import './App.css';
 export function App() {
   return (
     <div className="bg-amber-900 text-center">
