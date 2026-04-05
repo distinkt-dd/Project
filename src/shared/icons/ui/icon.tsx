c60184995a5fa41c616c4ac0993f8aaa449bab2e
z-u-s-h-o <zigy0ygiz@yandex.ru>
2026-04-05 16:57:41 +0500

    feat(ui-kit): добавила компонент управляющий иконками, настроила vite-plugin-svgr

---
+++ src/shared/icons/ui/icon.tsx
@@ -0,0 +1,66 @@
+import React, { type SVGProps } from 'react';
+import { type IconName, type IconProps } from './types';
+import {
+  ArrowLeftCircle,
+  ArrowNortheast,
+  Burger,
+  Check,
+  CloseCircle,
+  Close,
+  Down,
+  Edit,
+  EyeSlash,
+  Eye,
+  FullBrightness,
+  Grid,
+  Left,
+  Like,
+  Minus,
+  Moon,
+  Notifications,
+  Plus,
+  Right,
+  Search,
+  Trash,
+  Up,
+} from '../assets';
+
+const iconComponents: Record<IconName, React.FC<SVGProps<SVGSVGElement>>> = {
+  arrowLeftCircle: ArrowLeftCircle,
+  arrowNortheast: ArrowNortheast,
+  burger: Burger,
+  check: Check,
+  closeCircle: CloseCircle,
+  close: Close,
+  down: Down,
+  edit: Edit,
+  eyeSlash: EyeSlash,
+  eye: Eye,
+  fullBrightness: FullBrightness,
+  grid: Grid,
+  left: Left,
+  like: Like,
+  minus: Minus,
+  moon: Moon,
+  notifications: Notifications,
+  plus: Plus,
+  right: Right,
+  search: Search,
+  trash: Trash,
+  up: Up,
+};
+
+export const Icon: React.FC<IconProps> = ({
+  name,
+  size = 24,
+  color = 'currentColor',
+  ...props
+}) => {
+  const IconComponent = iconComponents[name];
+
+  if (!IconComponent) {
+    return null;
+  }
+
+  return <IconComponent width={size} height={size} color={color} {...props} />;
+};
---
+++ src/shared/icons/ui/types.ts
@@ -0,0 +1,31 @@
+import type { SVGProps } from 'react';
+
+export type IconName =
+  | 'arrowLeftCircle'
+  | 'arrowNortheast'
+  | 'burger'
+  | 'check'
+  | 'closeCircle'
+  | 'close'
+  | 'down'
+  | 'edit'
+  | 'eyeSlash'
+  | 'eye'
+  | 'fullBrightness'
+  | 'grid'
+  | 'left'
+  | 'like'
+  | 'minus'
+  | 'moon'
+  | 'notifications'
+  | 'plus'
+  | 'right'
+  | 'search'
+  | 'trash'
+  | 'up';
+
+export interface IconProps extends SVGProps<SVGSVGElement> {
+  name: IconName;
+  size?: number | string;
+  color?: string;
+}
