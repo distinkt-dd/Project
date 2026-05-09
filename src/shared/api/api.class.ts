f2df51190d211f93cbb58e4736e13a32932fb1ef
z-u-s-h-o <zigy0ygiz@yandex.ru>
2026-05-09 15:41:43 +0500

    feat(api): создан класс обертка над http методами

---
+++ src/shared/api/api.class.ts
@@ -0,0 +1,65 @@
+type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+export class Api {
+  readonly baseUrl: string;
+  protected options: RequestInit;
+
+  constructor(baseUrl: string, options: RequestInit = {}) {
+    this.baseUrl = baseUrl;
+    this.options = {
+      headers: {
+        'Content-Type': 'application/json',
+        ...((options.headers as object) ?? {}),
+      },
+    };
+  }
+
+  protected handleResponse<T>(response: Response): Promise<T> {
+    if (response.ok) return response.json();
+    else
+      return response
+        .json()
+        .then((data) => Promise.reject(data.error ?? response.statusText));
+  }
+
+  get<T extends object>(
+    uri: string,
+    params?: Record<string, string | number>
+  ): Promise<T> {
+    const queryString = params
+      ? '?' +
+        new URLSearchParams(
+          Object.fromEntries(
+            Object.entries(params).map(([k, v]) => [k, String(v)])
+          )
+        ).toString()
+      : '';
+    return fetch(this.baseUrl + uri + queryString, {
+      ...this.options,
+      method: 'GET',
+    }).then(this.handleResponse<T>);
+  }
+
+  post<T extends object>(
+    uri: string,
+    data: object,
+    method: ApiPostMethods = 'POST'
+  ): Promise<T> {
+    return fetch(this.baseUrl + uri, {
+      ...this.options,
+      method,
+      body: JSON.stringify(data),
+    }).then(this.handleResponse<T>);
+  }
+
+  put<T extends object>(uri: string, data: object): Promise<T> {
+    return this.post(uri, data, 'PUT');
+  }
+
+  delete<T extends object>(uri: string): Promise<T> {
+    return fetch(this.baseUrl + uri, {
+      ...this.options,
+      method: 'DELETE',
+    }).then(this.handleResponse<T>);
+  }
+}
