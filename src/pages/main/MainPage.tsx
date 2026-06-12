a545798ff389ff014266adad8f53d9e1e08dafdf
AlexanderGrankin <sacha.grankin.04@mail.ru>
2026-06-12 18:10:29 +0300

    feat: added Hero component

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,3 +1,10 @@
+import { Hero } from '@widgets/hero';
+import styles from './MainPage.module.css';
+
 export const MainPage: React.FC = () => {
-  return <div>MainPage</div>;
+  return (
+    <main className={styles.main}>
+      <Hero />
+    </main>
+  );
 };
