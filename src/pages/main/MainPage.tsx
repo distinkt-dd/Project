fe2f068d55abef89cda529729295248f4110039f
AlexanderGrankin <sacha.grankin.04@mail.ru>
2026-07-09 23:04:42 +0300

    feat: added weekly projects component

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,12 +1,12 @@
 import { Hero } from '@widgets/hero';
 import styles from './MainPage.module.css';
-import { ProjectsList } from '@widgets/projectsList';
+import { WeeklyProjects } from '@widgets/weeklyProjects';
 
 export const MainPage: React.FC = () => {
   return (
     <main className={styles.main}>
       <Hero />
-      <ProjectsList />
+      <WeeklyProjects />
     </main>
   );
 };
--- src/widgets/projectsList/ui/ProjectsList.tsx
+++ src/widgets/projectsList/ui/ProjectsList.tsx
@@ -1,52 +1,28 @@
 import { ProjectItem } from '@entities/project';
 import styles from './ProjectsList.module.css';
-import mock1 from './assets/mock1.png';
-import mock2 from './assets/mock2.png';
-import mock3 from './assets/mock3.png';
 
-const mock_projects = [
-  {
-    id: 1,
-    name: 'Создание анимационного фильма',
-    image: mock1,
-    tags: [
-      'графический дизайнер',
-      'художник',
-      'AI-дизайнер',
-      'front разработчик',
-      'маркетолог',
-    ],
-  },
-  {
-    id: 2,
-    name: 'Сайт приюта для бездомных животных',
-    image: mock2,
-    tags: [
-      'веб-дизайнер',
-      'маркетолог',
-      'back разработчик',
-      'front разработчик',
-    ],
-  },
-  {
-    id: 3,
-    name: 'Визуальная новелла «По ту сторону»',
-    image: mock3,
-    tags: ['графический дизайнер', 'маркетолог', 'геймдевелопер'],
-  },
-];
+interface Project {
+  id: number;
+  name: string;
+  image: string;
+  tags: string[];
+}
 
-export const ProjectsList: React.FC = () => {
+interface ProjectsListProps {
+  data: Project[];
+}
+
+export const ProjectsList: React.FC<ProjectsListProps> = ({ data }) => {
   return (
     <div className={styles.container}>
       <ul className={styles.list}>
-        {mock_projects.map((proj, i) => (
+        {data.map((proj) => (
           <ProjectItem
             id={proj.id}
             name={proj.name}
             image={proj.image}
             tags={proj.tags}
-            key={i}
+            key={proj.id}
           />
         ))}
       </ul>
