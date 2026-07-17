178573b299a81aaf0d80b3b51d15bf960b0df8bc
z-u-s-h-o <zigy0ygiz@yandex.ru>
2026-07-17 17:40:29 +0500

    feat(general): добавлен Redux провайдер в main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
-import './styles/index.css';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
